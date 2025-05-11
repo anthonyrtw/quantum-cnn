a0f8f8ad7cb6c68733ddd76a099274030c1f93ab
Anthony Walsh <anthonywalshrt@gmail.com>
2025-05-11 17:03:51 +0200

    Debug QCNN layers. Reformat script.

--- src.py
+++ src.py
@@ -17,11 +17,11 @@ class OneHotAmplitude(Encoder):
     def __init__(self, dims):
         super().__init__()
 
-        self._dims = dims
+        self.dims = dims
 
         # Define basis states for each register.
-        basisX = torch.eye(self._dims[0])
-        basisY = torch.eye(self._dims[1])
+        basisX = torch.eye(self.dims[0])
+        basisY = torch.eye(self.dims[1])
 
         # Get cartesian product of indices
         idx_x, idx_y = torch.arange(basisX.size(0)), torch.arange(basisY.size(0))
@@ -30,21 +30,21 @@ class OneHotAmplitude(Encoder):
         basis_states = torch.cat(
             (basisX[grid_x.flatten()], basisY[grid_y.flatten()]), dim=1
         ) 
+        zero = torch.tensor([1, 0]) 
+        one = torch.tensor([0, 1])
 
         # Vectorize this later. States must be given as tensor products.
         self._basis_states = torch.zeros(len(basis_states), 2 ** len(basis_states[0]))
         for i, vec in enumerate(basis_states):
             kron_ = torch.tensor([1])
             for val in vec:
-                if val:
-                    kron_ = torch.kron(kron_, torch.tensor([0, 1]))
-                else:
-                    kron_ = torch.kron(kron_, torch.tensor([1, 0]))
+                kron_ = torch.kron(kron_, one if val else zero)
+
             self._basis_states[i] = kron_
 
     def forward(self, qdev, X):
-        if qdev.n_wires != sum(self._dims):
-            raise ValueError('Quantum Device does not match image dimensions.')
+        if qdev.n_wires != sum(self.dims):
+            raise ValueError(f'Quantum Device does not match image dimensions. {qdev.n_wires} != {sum(self.dims)}')
                 
         if len(X.shape) != 3:
             X = X.unsqueeze(0)
@@ -63,7 +63,7 @@ class OneHotAmplitude(Encoder):
         qdev.set_states(X_state)
 
     def __repr__(self):
-        return f'OneHotAmplitude({self._dims[0]}, {self._dims[1]})'
+        return f'OneHotAmplitude({self.dims[0]}, {self.dims[1]})'
 
 
 class RBS(QuantumModule):
@@ -77,23 +77,20 @@ class RBS(QuantumModule):
         super().__init__()
 
         # Instantiate theta as a torch tensor
-        if isinstance(theta, float):
-            self._theta = nn.Parameter(torch.tensor([theta]),)
-        elif isinstance(theta, torch.Tensor):
-            self._theta = nn.Parameter(theta)
-        else:
-            self._theta = 2 * np.pi * torch.rand(1)
+        if theta is None:
+            theta = 2 * np.pi * torch.rand(1)
 
-        self._wires = wires
+        self.theta = torch.nn.Parameter(theta)
+        self.wires = wires
 
     def forward(self, qdev: QuantumDevice, wires = None):
-        if wires is None and self._wires is None:
+        if wires is None and self.wires is None:
             raise ValueError("Please specify wires to place RBS gate.")
 
-        wires = wires if wires is not None else self._wires
+        wires = wires if wires is not None else self.wires
 
-        c = torch.cos(self._theta).item()
-        s = torch.sin(self._theta).item()
+        c = torch.cos(self.theta).item()
+        s = torch.sin(self.theta).item()
 
         qdev.qubitunitary(
             wires=wires, 
@@ -104,8 +101,8 @@ class RBS(QuantumModule):
         )
 
     def __repr__(self):
-        if self._wires is not None:
-            return f'RBS(wires=[{self._wires[0]}, {self._wires[1]}])'
+        if self.wires is not None:
+            return f'RBS(wires=[{self.wires[0]}, {self.wires[1]}])'
         else:
             return 'RBS'
 
@@ -118,31 +115,35 @@ class Conv2dFilter(QuantumModule):
     :param kernel_size: Size of the convolutional filter
     """
     def __init__(self, kernel_size):
-        super().__init__()
+        super(Conv2dFilter, self).__init__()
 
-        self._kernel_size = kernel_size
+        self.kernel_size = kernel_size
+        num_rbs = (self.kernel_size // 2) * ((self.kernel_size + 1) // 2) + \
+            ((self.kernel_size - 1) // 2) * ((self.kernel_size - 1) // 2)
+
+        # Instantiate RBS gates
+        self._rbs_list = torch.nn.ModuleList([RBS() for i in range(num_rbs)])
+
+        # Determine wires to apply each RBS gate
+        even_wires = [[2 * i, 2 * i + 1] for i in range(self.kernel_size // 2)] 
+        odd_wires = [[2 * i + 1, 2 * i + 2] for i in range((self.kernel_size - 1) // 2)]
+
+        rbs_wires = []
+        for col in range((2 * self.kernel_size - 3)):
+            wires = odd_wires if col % 2 else even_wires
+            rbs_wires += wires
+
+        self._rbs_wires = np.array(rbs_wires)
+
 
     def forward(self, qdev, start_wire = None):
-        wires1 = [[2 * i + start_wire, 2 * i + 1 + start_wire] for i in range(self._kernel_size // 2)
-                    for i in range(self._kernel_size // 2)]
-        
-        wires2 = [[2 * i + 1 + start_wire, 2 * i + 2 + start_wire] 
-                  for i in range((self._kernel_size - 1) // 2)]
-
-        if wires2:
-            self._rbs_list = [
-                RBS(wires=w) for w in (wires1 + wires2) 
-                for column in range(self._kernel_size // 2 + 1)
-            ]
-        else:
-            # For kernel size = 2: Single RBS gate
-            self._rbs_list = [RBS(wires=wires1[0])]
+        rbs_wires = self._rbs_wires + start_wire
 
         for i, rbs in enumerate(self._rbs_list):
-            rbs(qdev)
+            rbs(qdev, rbs_wires[i])
 
     def __repr__(self):
-        return f'Conv2dFilter(kernel_size={self._kernel_size})'
+        return f'Conv2dFilter(kernel_size={self.kernel_size})'
 
 
 class Conv2d(QuantumModule):
@@ -158,22 +159,22 @@ class Conv2d(QuantumModule):
     def __init__(self, dims: tuple, kernel_size: int | tuple[int], stride: int | tuple[int] = None):
         super().__init__()
 
-        self._dims = dims
-        self._kernel_size = (kernel_size, kernel_size) if isinstance(kernel_size, int) else kernel_size
+        self.dims = dims
+        self.kernel_size = (kernel_size, kernel_size) if isinstance(kernel_size, int) else kernel_size
 
         stride = kernel_size if stride is None else stride
-        self._stride = (stride, stride) if isinstance(stride, int) else stride    
+        self.stride = (stride, stride) if isinstance(stride, int) else stride    
 
-        self._filterX = Conv2dFilter(self._kernel_size[0])
-        self._filterY = Conv2dFilter(self._kernel_size[1])
+        self._filterX = Conv2dFilter(self.kernel_size[0])
+        self._filterY = Conv2dFilter(self.kernel_size[1])
 
         self._start_wires_x = [
-            i * self._stride[0] for i in range((dims[0] - self._kernel_size[0] + self._stride[0]) // self._stride[0])
-            if i * self._stride[0] < dims[0]
+            i * self.stride[0] for i in range((dims[0] - self.kernel_size[0] + self.stride[0]) // self.stride[0])
+            if i * self.stride[0] < dims[0]
         ]
         self._start_wires_y = [
-            i * self._stride[1] + dims[0] for i in range((dims[1] - self._kernel_size[1] + self._stride[1]) // self._stride[1])
-            if i * self._stride[1] < sum(dims) - self._kernel_size[1]
+            i * self.stride[1] + dims[0] for i in range((dims[1] - self.kernel_size[1] + self.stride[1]) // self.stride[1])
+            if i * self.stride[1] < sum(dims) - self.kernel_size[1]
         ]
     
     def forward(self, qdev):
@@ -184,7 +185,7 @@ class Conv2d(QuantumModule):
             self._filterY(qdev, start_wire=wire)
 
     def __repr__(self):
-        return f'Conv2d(dims={self._dims}, kernel_size={self._kernel_size}, stride={self._stride})'
+        return f'Conv2d(dims={self.dims}, kernel_size={self.kernel_size}, stride={self.stride})'
 
 
 class Pooling(QuantumModule):
@@ -199,13 +200,13 @@ class Pooling(QuantumModule):
     def __init__(self, dims: tuple[int], kernel_size: int | tuple[int] = 2):
         super().__init__()
 
-        self._dims = dims
+        self.dims = dims
         if isinstance(kernel_size, int):
-            self._kernel_size = (kernel_size, kernel_size)
+            self.kernel_size = (kernel_size, kernel_size)
         else:
-            self._kernel_size = kernel_size
+            self.kernel_size = kernel_size
 
-        if (dims[0] % self._kernel_size[0]) or (dims[1] % self._kernel_size[1]):
+        if (dims[0] % self.kernel_size[0]) or (dims[1] % self.kernel_size[1]):
             raise ValueError("The dimension of the image should be divisible by the kernel size.")
         
         # Determine wires in each register to perform CNOT gates
@@ -221,8 +222,8 @@ class Pooling(QuantumModule):
         ]
         
         # The following permutation arranges the control qubits to the bottom of the register and target qubits to the top
-        self._swaps = [[i, self._kernel_size[0] * i] for i in range(1, dims[0] // self._kernel_size[0])]
-        self._swaps += [[dims[0] // self._kernel_size[0] + i, dims[0] + self._kernel_size[1] * i] for i in range(dims[1] // self._kernel_size[1])]
+        self._swaps = [[i, self.kernel_size[0] * i] for i in range(1, dims[0] // self.kernel_size[0])]
+        self._swaps += [[dims[0] // self.kernel_size[0] + i, dims[0] + self.kernel_size[1] * i] for i in range(dims[1] // self.kernel_size[1])]
 
     def forward(self, qdev):
         # Pool X register
@@ -240,7 +241,7 @@ class Pooling(QuantumModule):
             qdev.swap(swap)
 
     def __repr__(self):
-        return f'Pooling(dims={self._dims}, kernel_size={self._kernel_size})'
+        return f'Pooling(dims={self.dims}, kernel_size={self.kernel_size})'
 
 
 class PyramidDense(QuantumModule):
@@ -254,24 +255,24 @@ class PyramidDense(QuantumModule):
     def __init__(self, n_wires: int):
         super().__init__() 
 
-        self._n_wires = n_wires
+        self.n_wires = n_wires
 
         wires_list = []
-        for col in range(2, n_wires):
+        for col in range(2 * n_wires - 3):
             if 2 * (col - 1) + 1 < n_wires:
-                wires_list += [[2 * i, 2 * i + 1] for i in range(col - 1)]
+                wires_list += [[2 * i, 2 * i + 1] for i in range(col)]
             if 2 * (col - 1) + 2 < n_wires:
-                wires_list += [[2 * i + 1, 2 * i + 2] for i in range(col - 1)]
+                wires_list += [[2 * i + 1, 2 * i + 2] for i in range(col)]
 
         wires_list += list(reversed(wires_list[:-1]))
-        self._rbs_list = [RBS(wires=wires) for wires in wires_list]
+        self._rbs_list = torch.nn.ModuleList([RBS(wires=wires) for wires in wires_list])
 
     def forward(self, qdev):
         for rbs in self._rbs_list:
             rbs(qdev)
 
     def __repr__(self):
-        return f'PyramidDense(n_wires={self._n_wires})'
+        return f'PyramidDense(n_wires={self.n_wires})'
 
 
 class MeasureLayer(torch.nn.Module):
@@ -283,34 +284,34 @@ class MeasureLayer(torch.nn.Module):
     def __init__(self, n_wires, n_shots = None):
         super().__init__()
 
-        self._n_wires = n_wires
-        self._n_shots = n_shots if n_shots is not None else 1024 
+        self.n_wires = n_wires
+        self.n_shots = n_shots if n_shots is not None else 1024 
 
         self._all_measurements = list(self._generate_all_measurements())
 
 
     def forward(self, qdev):
-        results = measure(qdev, n_shots=self._n_shots)
-        output = torch.zeros(len(results), int(comb(self._n_wires, 2)))
+        results = measure(qdev, n_shots=self.n_shots)
+        output = torch.zeros(len(results), int(comb(self.n_wires, 2)))
 
         indices = {key: i for i, key in enumerate(self._all_measurements)}
 
         for i, res in enumerate(results):
             for key, count in res.items():
-                trimmed_key = key[:self._n_wires]
+                trimmed_key = key[:self.n_wires]
                 if trimmed_key in self._all_measurements:
                     output[i][indices[trimmed_key]] += count
         return output
     
     def _generate_all_measurements(self):
         """Generate all possible bit strings with two 1s."""
-        if self._n_wires < 2:
+        if self.n_wires < 2:
             return
-        for i, j in combinations(range(self._n_wires), 2):
-            bit_string = ['0'] * self._n_wires
+        for i, j in combinations(range(self.n_wires), 2):
+            bit_string = ['0'] * self.n_wires
             bit_string[i] = '1'
             bit_string[j] = '1'
             yield ''.join(bit_string)
     
     def __repr__(self):
-        return f"Measure(n_wires={self._n_wires})"
+        return f"Measure(n_wires={self.n_wires})"
