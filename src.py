21ead20ea7838427266f9ebe790ce82700e8701b
Anthony Walsh <anthonywalshrt@gmail.com>
2025-05-18 11:52:06 +0200

    Refactor QCNN layers for vectorized format. Rename layers.

--- src.py
+++ src.py
@@ -2,10 +2,9 @@ import torch
 import torch.nn as nn
 
 import numpy as np
-from scipy.special import comb
 from itertools import combinations
 
-from torchquantum import QuantumModule, QuantumDevice, partial_trace, matrix_form
+from torchquantum import QuantumModule, QuantumDevice, CNOT, SWAP, partial_trace, matrix_form
 from torchquantum.encoding import Encoder
 
 class OneHotAmplitude(Encoder):
@@ -16,24 +15,23 @@ class OneHotAmplitude(Encoder):
     """
     def __init__(self, dims):
         super().__init__()
-
         self.dims = dims
 
         # Define basis states for each register.
-        basisX = torch.eye(self.dims[0])
-        basisY = torch.eye(self.dims[1])
+        basis_x = torch.eye(self.dims[0])
+        basis_y = torch.eye(self.dims[1])
 
         # Get cartesian product of indices
-        idx_x, idx_y = torch.arange(basisX.size(0)), torch.arange(basisY.size(0))
+        idx_x, idx_y = torch.arange(basis_x.size(0)), torch.arange(basis_y.size(0))
         grid_x, grid_y = torch.meshgrid(idx_x, idx_y, indexing='ij')
 
         basis_states = torch.cat(
-            (basisX[grid_x.flatten()], basisY[grid_y.flatten()]), dim=1
+            (basis_x[grid_x.flatten()], basis_y[grid_y.flatten()]), dim=1
         ) 
         zero = torch.tensor([1, 0]) 
         one = torch.tensor([0, 1])
 
-        # Vectorize this later. States must be given as tensor products.
+        # States must be given as tensor products.
         self._basis_states = torch.zeros(len(basis_states), 2 ** len(basis_states[0]))
         for i, vec in enumerate(basis_states):
             kron_ = torch.tensor([1])
@@ -73,78 +71,36 @@ class RBS(QuantumModule):
     :param theta: RBS gate angle.
     :param wires: wires to apply the RBS between.
     """
-    def __init__(self, theta = None, wires = None):
+    def __init__(self, wires: list[int], theta: torch.nn.Parameter = None):
         super().__init__()
 
         # Instantiate theta as a torch tensor
         if theta is None:
             theta = 2 * np.pi * torch.rand(1)
-
-        self.theta = torch.nn.Parameter(theta)
+            self.theta = torch.nn.Parameter(theta)
+        else:
+            self.theta = theta
+        
         self.wires = wires
 
-    def forward(self, qdev: QuantumDevice, wires = None):
-        if wires is None and self.wires is None:
-            raise ValueError("Please specify wires to place RBS gate.")
+    def forward(self, qdev: QuantumDevice):
 
-        wires = wires if wires is not None else self.wires
-
-        c = torch.cos(self.theta).item()
-        s = torch.sin(self.theta).item()
+        c = torch.cos(self.theta)
+        s = torch.sin(self.theta)
 
         qdev.qubitunitary(
-            wires=wires, 
+            wires=self.wires, 
             params=[[1, 0, 0, 0],
                     [0, c, s, 0],
                     [0, -s, c, 0],
                     [0, 0, 0, 1]]
         )
+        return qdev
 
     def __repr__(self):
-        if self.wires is not None:
-            return f'RBS(wires=[{self.wires[0]}, {self.wires[1]}])'
-        else:
-            return 'RBS'
+        return f'RBS(wires=[{self.wires[0]}, {self.wires[1]}])'
 
-class Conv2dFilter(QuantumModule):
-    """ 
-    2D Convolutional filter applied to a register within a convolutional layer.
-    Each convolutional filter consists of a series of RBS gates applied between 
-    each pair of qubits in the register.
-
-    :param kernel_size: Size of the convolutional filter
-    """
-    def __init__(self, kernel_size):
-        super(Conv2dFilter, self).__init__()
-
-        self.kernel_size = kernel_size
-        num_rbs = (self.kernel_size // 2) * ((self.kernel_size + 1) // 2) + \
-            ((self.kernel_size - 1) // 2) * ((self.kernel_size - 1) // 2)
-
-        # Instantiate RBS gates
-        self._rbs_list = torch.nn.ModuleList([RBS() for i in range(num_rbs)])
-
-        # Determine wires to apply each RBS gate
-        even_wires = [[2 * i, 2 * i + 1] for i in range(self.kernel_size // 2)] 
-        odd_wires = [[2 * i + 1, 2 * i + 2] for i in range((self.kernel_size - 1) // 2)]
-
-        rbs_wires = []
-        for col in range((2 * self.kernel_size - 3)):
-            wires = odd_wires if col % 2 else even_wires
-            rbs_wires += wires
-
-        self._rbs_wires = np.array(rbs_wires)
-
-
-    def forward(self, qdev, start_wire = None):
-        rbs_wires = self._rbs_wires + start_wire
-
-        for i, rbs in enumerate(self._rbs_list):
-            rbs(qdev, rbs_wires[i])
-
-    def __repr__(self):
-        return f'Conv2dFilter(kernel_size={self.kernel_size})'
-
+    
 
 class Conv2d(QuantumModule):
     """ 
@@ -153,7 +109,7 @@ class Conv2d(QuantumModule):
     
     :param kernel_size: Size of the convolutional filter
     :param stride: Vertical distance the convolutional filter shifts down the register.
-        Note: for a stride < kernel_size, convolutions are applied successively to the 
+        **Note**: for a stride < kernel_size, convolutions are applied successively to the 
         same set of qubits and translational invariance is not preserved.
     """
     def __init__(self, dims: tuple, kernel_size: int | tuple[int], stride: int | tuple[int] = None):
@@ -163,29 +119,73 @@ class Conv2d(QuantumModule):
         self.kernel_size = (kernel_size, kernel_size) if isinstance(kernel_size, int) else kernel_size
 
         stride = kernel_size if stride is None else stride
-        self.stride = (stride, stride) if isinstance(stride, int) else stride    
+        self.stride = (stride, stride) if isinstance(stride, int) else stride
 
-        self._filterX = Conv2dFilter(self.kernel_size[0])
-        self._filterY = Conv2dFilter(self.kernel_size[1])
+        theta_x = 2 * torch.pi * torch.rand(2 * self.kernel_size[0] - 3)
+        theta_y = 2 * torch.pi * torch.rand(2 * self.kernel_size[1] - 3)
+        self._thetaX = torch.nn.Parameter(theta_x)
+        self._thetaY = torch.nn.Parameter(theta_y)
 
-        self._start_wires_x = [
+        start_wires_x = [
             i * self.stride[0] for i in range((dims[0] - self.kernel_size[0] + self.stride[0]) // self.stride[0])
             if i * self.stride[0] < dims[0]
         ]
-        self._start_wires_y = [
+        start_wires_y = [
             i * self.stride[1] + dims[0] for i in range((dims[1] - self.kernel_size[1] + self.stride[1]) // self.stride[1])
             if i * self.stride[1] < sum(dims) - self.kernel_size[1]
         ]
+        self._filters = [
+            self._Filter(self.kernel_size[0], theta_x, start_wire)
+            for start_wire in start_wires_x
+        ] + [
+            self._Filter(self.kernel_size[1], theta_y, start_wire)
+            for start_wire in start_wires_y
+        ]
+        self.network = torch.nn.Sequential(*self._filters)
     
     def forward(self, qdev):
-        for wire in self._start_wires_x:
-            self._filterX(qdev, start_wire=wire)
-        
-        for wire in self._start_wires_y:
-            self._filterY(qdev, start_wire=wire)
+        self.network(qdev)
+        #return qdev
 
     def __repr__(self):
         return f'Conv2d(dims={self.dims}, kernel_size={self.kernel_size}, stride={self.stride})'
+
+    class _Filter(QuantumModule):
+        """ 
+        2D Convolutional filter applied to a register within a convolutional layer.
+        Each convolutional filter consists of a series of RBS gates applied between 
+        each pair of qubits in the register.
+
+        :param kernel_size: Size of the convolutional filter
+        :param theta: torch.Parameter that deterimines the RBS values of the filter.
+        """
+        def __init__(self, kernel_size: int, theta: torch.nn.Parameter, start_wire: int):
+            super().__init__()
+            self.kernel_size = kernel_size
+            self.start_wire = start_wire
+
+            # Determine wires to apply each RBS gate
+            even_wires = [[2 * i, 2 * i + 1] for i in range(kernel_size // 2)] 
+            odd_wires = [[2 * i + 1, 2 * i + 2] for i in range((kernel_size - 1) // 2)]
+
+            rbs_wires = []
+            for col in range((2 * kernel_size - 3)):
+                wires = odd_wires if col % 2 else even_wires
+                rbs_wires += wires
+
+            rbs_wires = np.array(rbs_wires) + start_wire
+
+            # Instantiate RBS gates
+            rbs_list = [RBS(wires, theta[i]) for i, wires in enumerate(rbs_wires)]
+
+            self.network = torch.nn.Sequential(*rbs_list)
+
+        def forward(self, qdev):
+            self.network(qdev)
+            return qdev
+
+        def __repr__(self):
+            return f'Conv2dFilter(kernel_size={self.kernel_size}, start_wire={self.start_wire})'
 
 
 class Pooling(QuantumModule):
@@ -210,35 +210,40 @@ class Pooling(QuantumModule):
             raise ValueError("The dimension of the image should be divisible by the kernel size.")
         
         # Determine wires in each register to perform CNOT gates
-        registerX, registerY = list(range(dims[0])), list(range(dims[0], dims[0] + dims[1]))
+        register_x, register_y = list(range(dims[0])), list(range(dims[0], dims[0] + dims[1]))
 
         self._registerX_pooled = [
-            registerX[i:i + kernel_size][::-1]
-            for i in range(0, len(registerX), kernel_size)
+            register_x[i:i + kernel_size][::-1]
+            for i in range(0, len(register_x), kernel_size)
         ]
         self._registerY_pooled = [
-            registerY[i:i + kernel_size][::-1]
-            for i in range(0, len(registerY), kernel_size)
+            register_y[i:i + kernel_size][::-1]
+            for i in range(0, len(register_y), kernel_size)
         ]
-        
-        # The following permutation arranges the control qubits to the bottom of the register and target qubits to the top
-        self._swaps = [[i, self.kernel_size[0] * i] for i in range(1, dims[0] // self.kernel_size[0])]
-        self._swaps += [[dims[0] // self.kernel_size[0] + i, dims[0] + self.kernel_size[1] * i] for i in range(dims[1] // self.kernel_size[1])]
 
-    def forward(self, qdev):
-        # Pool X register
+        cnots = []
         for pool in reversed(self._registerX_pooled):
             for i in range(len(pool) - 1):
-                qdev.cx([pool[i], pool[i + 1]])
-
-        # Pool Y register
+                cnots.append(CNOT(wires=[pool[i], pool[i + 1]]))
+        
         for pool in reversed(self._registerY_pooled):
             for i in range(len(pool) - 1):
-                qdev.cx([pool[i], pool[i + 1]])
+                cnots.append(CNOT(wires=[pool[i], pool[i + 1]])) 
+        
+        # The following permutation arranges the control qubits to the bottom of the register and target qubits to the top
+        swaps_wires = [[i, self.kernel_size[0] * i] for i in range(1, dims[0] // self.kernel_size[0])]
+        swaps_wires += [
+            [dims[0] // self.kernel_size[0] + i, dims[0] + self.kernel_size[1] * i] 
+            for i in range(dims[1] // self.kernel_size[1])
+        ]
 
-        # Drop control qubits to bottom of register
-        for swap in self._swaps:
-            qdev.swap(swap)
+        swaps = [SWAP(wires=wires) for wires in swaps_wires]
+
+        self.ops = cnots + swaps
+
+    def forward(self, qdev):
+        for op in self.ops:
+            op(qdev)
 
     def __repr__(self):
         return f'Pooling(dims={self.dims}, kernel_size={self.kernel_size})'
@@ -265,11 +270,12 @@ class PyramidDense(QuantumModule):
                 wires_list += [[2 * i + 1, 2 * i + 2] for i in range(col)]
 
         wires_list += list(reversed(wires_list[:-1]))
-        self._rbs_list = torch.nn.ModuleList([RBS(wires=wires) for wires in wires_list])
+        rbs_list = [RBS(wires=wires) for wires in wires_list]
+
+        self.network = torch.nn.Sequential(*rbs_list)
 
     def forward(self, qdev):
-        for rbs in self._rbs_list:
-            rbs(qdev)
+        self.network(qdev)
 
     def __repr__(self):
         return f'PyramidDense(n_wires={self.n_wires})'
